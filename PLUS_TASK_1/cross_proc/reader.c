6cbd8cd9318530d23a9e7b217b4f4f11114baeab
synthMoza <eganian.aa@phystech.edu>
2020-11-09 18:40:45 +0300

    Switch is completed, bug is still there

--- PLUS_TASK_1/cross_proc/reader.c
+++ PLUS_TASK_1/cross_proc/reader.c
@@ -13,64 +13,37 @@ int main() {
 	char buf[BUF_SIZE] = {0};
 	char cross_proc_s[CROSS_PROC_FLENGTH] = {0};
 	int mark = MARK;
+	int cur_pid = 0;
 
-	// select vars
-	fd_set readfds;
-	struct timeval timeout;
-	timeout.tv_sec = TIMEOUT_SEC;
-	timeout.tv_usec = 0;
+	cur_pid = getpid();
+	// Create and open target fifo in nonblocking mode
+	tostring(cur_pid, cross_proc_s);
+	if (mkfifo(cross_proc_s, PERMISSIONS) == -1) {
+		perror("");
+		printf("Writer: can't create target fifo!\n");
+		exit(-1);
+	}
+	target_fifo = open(cross_proc_s, O_RDONLY | O_NONBLOCK);
 
-	// Create cross_proc fifo (if it doesn't exist)
+	// Create shared fifo
 	if (mkfifo(CROSS_PROC, PERMISSIONS) == -1 && errno != EEXIST) {
 		perror("");
 		printf("Reader: can't create CROSS_PROC fifo!\n");
 		exit(-1);
 	}
 
-	// Open cross proc fifo in nonblocking mode
-	cross_proc = open(CROSS_PROC, O_RDONLY | O_NONBLOCK);
-	if (cross_proc == -1) {
-		printf("Reader: can't open cross proc fifo!\n");
-		exit(-1);
-	}
+	// Wait for the writer to join this reader by openning in block mode
+	cross_proc = open(CROSS_PROC, O_WRONLY);
 
-	sleep(5);
-
-	FD_ZERO(&readfds);
-	FD_SET(cross_proc, &readfds);
-
-	if (select(cross_proc + 1, &readfds, NULL, NULL, &timeout) != 1) {
+	// Write current process's pid into cross_proc fifo
+	if (write(cross_proc, &cur_pid, sizeof(pid_t)) == -1) {
 		perror("");
-		printf("Select error!\n");
-		exit(-1);
-	}
-	// Read the target fifo's name from cross_proc fifo
-	read_bytes = read(cross_proc, &read_pid, sizeof(pid_t));
-
-	if (read_bytes == -1 || read_bytes == 0) {
-		perror("");
-		printf("Reader: error reading writer's pid!\n");
-		exit(-1);
-	}
-
-	// Open target fifo
-	tostring(read_pid, cross_proc_s);
-	target_fifo = open(cross_proc_s, O_RDWR | O_NONBLOCK);
-	if (target_fifo == -1) {
-		perror("");
-		printf("Reader: can't open the target fifo!\n");
+		printf("Reader: error writing pid into fifo\n");
 		exit(-1);
 	}
 
 	flags = fcntl(target_fifo, F_GETFL, 0);
 	flags = fcntl(target_fifo, F_SETFL, flags & ~O_NONBLOCK);
-
-	// Send reader state mark (pipe was opened for reading)
-	// if (write(target_fifo, &mark, sizeof(int)) != sizeof(int)) {
-	// 	perror("");
-	// 	printf("Reader: can't send state mark\n");
-	// 	exit(-1);
-	// }
 
 	while ((read_bytes = read(target_fifo, buf, BUF_SIZE)) == BUF_SIZE) {
 		write(STDOUT_FILENO, buf, BUF_SIZE);
--- PLUS_TASK_1/cross_proc/writer.c
+++ PLUS_TASK_1/cross_proc/writer.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]) {
 	char cross_proc_s[CROSS_PROC_FLENGTH] = {0};
 	char buf[BUF_SIZE] = {0};
 	int mark = 0;
+	pid_t read_pid = 0;
 
 	int read_bytes = 0;
 
@@ -18,17 +19,6 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
-	cur_pid = getpid();
-
-	// Create and open target fifo in nonblocking mode
-	tostring(cur_pid, cross_proc_s);
-	if (mkfifo(cross_proc_s, PERMISSIONS) == -1) {
-		perror("");
-		printf("Writer: can't create target fifo!\n");
-		exit(-1);
-	}
-	target_fifo = open(cross_proc_s, O_RDWR | O_NONBLOCK);
-
 	// Create shared fifo
 	if (mkfifo(CROSS_PROC, PERMISSIONS) == -1 && errno != EEXIST) {
 		perror("");
@@ -36,8 +26,26 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
-	// Wait for the reader to join this writer by openning in block mode
-	cross_proc = open(CROSS_PROC, O_WRONLY);
+	// Wait for the writer to join this reader by openning in block mode
+	cross_proc = open(CROSS_PROC, O_RDONLY);
+
+	// Read the target fifo's name from cross_proc fifo
+	read_bytes = read(cross_proc, &read_pid, sizeof(pid_t));
+
+	if (read_bytes == -1 || read_bytes == 0) {
+		perror("");
+		printf("Writer: error reading writer's pid!\n");
+		exit(-1);
+	}
+
+	// Open target fifo
+	tostring(read_pid, cross_proc_s);
+	target_fifo = open(cross_proc_s, O_WRONLY | O_NONBLOCK);
+	if (target_fifo == -1) {
+		perror("");
+		printf("Writer: can't open the target fifo!\n");
+		exit(-1);
+	}
 
 	// Open input file
 	file = open(argv[1], O_RDONLY);
@@ -46,29 +54,8 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
-	// Write current process's pid into cross_proc fifo
-	if (write(cross_proc, &cur_pid, sizeof(pid_t)) == -1) {
-		perror("");
-		printf("Writer: error writing pid into fifo\n");
-		exit(-1);
-	}
-
 	flags = fcntl(target_fifo, F_GETFL, 0);
 	flags = fcntl(target_fifo, F_SETFL, flags & ~O_NONBLOCK);
-
-	// printf("CHECK\n");
-	// fflush(stdout);
-
-	// // Check the state of reader
-	// read_bytes = read(target_fifo, &mark, sizeof(int));
-	// if (read_bytes != sizeof(int)) {
-	// 	perror("");
-	// 	printf("Writer: can't get reader state mark\n");
-	// 	exit(-1);
-	// }
-
-	// printf("CHECK\n");
-	// fflush(stdout);
 
 	// Write everything into fifo from the input file
 	while ((read_bytes = read(file, &buf, BUF_SIZE)) == BUF_SIZE) {
