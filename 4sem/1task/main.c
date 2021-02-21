bec5dd8d1db38d41d1738b410f59f4047e3d54e2
synthMoza <eganian.aa@phystech.edu>
2021-02-22 02:00:46 +0300

    Add main.c

--- 4sem/1task/main.c
+++ 4sem/1task/main.c
@@ -1,18 +1,6 @@
 #include "avltree.h"
 
 int main() {
-    Node* tree = NULL;
-    int n = 0;
-    int elem = 0;
-
-    scanf("%d", &n);
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &elem);
-        tree = nodeInsert(tree, elem);
-    }
-
-    nodePrint(tree, LNR);
-
-    nodeDestroy(tree);
+    
     return 0;
 }
